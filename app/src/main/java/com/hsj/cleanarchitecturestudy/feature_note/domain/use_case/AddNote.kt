b36657145a79469909d4eefcbf05465ee5c01f38
JoHongsik <queue1991@naver.com>
2022-03-16 22:21:26 +0900

    add 1. NotesViewModel

---
+++ app/src/main/java/com/hsj/cleanarchitecturestudy/feature_note/domain/use_case/AddNote.kt
@@ -0,0 +1,23 @@
+package com.hsj.cleanarchitecturestudy.feature_note.domain.use_case
+
+import com.hsj.cleanarchitecturestudy.feature_note.domain.model.InvalidNoteException
+import com.hsj.cleanarchitecturestudy.feature_note.domain.model.Note
+import com.hsj.cleanarchitecturestudy.feature_note.domain.repository.NoteRepository
+
+class AddNote(
+    private val repository:NoteRepository
+) {
+
+    @Throws(InvalidNoteException::class)
+    suspend operator fun invoke(note: Note) {
+        if(note.title.isBlank()){
+            throw InvalidNoteException("The title of the note can't be empty.")
+        }
+
+        if(note.content.isBlank()){
+            throw InvalidNoteException("The content of the note can't be empty.")
+        }
+
+        repository.insertNote(note)
+    }
+}
